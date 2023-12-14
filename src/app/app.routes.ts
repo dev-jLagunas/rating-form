a982eb07edb4f436279752f9481532989983a797
Juan Lagunas <dev.jlagunas@gmail.com>
2023-12-14 12:38:18 +0900

    Components and routes created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RatingFormComponent } from './rating-form/rating-form.component';
+import { ThanksPageComponent } from './thanks-page/thanks-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: RatingFormComponent },
+  { path: 'thanks', component: ThanksPageComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
---
+++ src/app/rating-form/rating-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rating-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './rating-form.component.html',
+  styleUrl: './rating-form.component.scss'
+})
+export class RatingFormComponent {
+
+}
